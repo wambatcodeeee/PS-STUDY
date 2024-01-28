63350af814e824eb4955d890da91d1baa6e3ae18
JoonYong Lim <feyfey927@gmail.com>
2024-01-29 03:21:47 +0900

    [Bronze IV] Title: R2, Time: 124 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int R1 = Integer.parseInt(st.nextToken());
+        int avg = Integer.parseInt(st.nextToken());
+
+        System.out.println(avg * 2 - R1);
+    }
+}
