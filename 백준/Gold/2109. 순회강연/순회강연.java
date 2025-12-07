94feb27611714fc2ba6cd49382e31bba773671e8
JoonYong Lim <feyfey927@gmail.com>
2025-12-08 02:04:42 +0900

    [Gold III] Title: 순회강연, Time: 240 ms, Memory: 20468 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -1,53 +1,50 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
+import java.io.*;
+
+class Lecture implements Comparable<Lecture>{
+    int pay;
+    int day;
+
+    public Lecture(int pay, int day){
+        this.pay = pay;
+        this.day = day;
+    }
+
+    @Override
+    public int compareTo(Lecture o1){
+        return this.day - o1.day;
+    }
+}
 
 public class Main {
-    static class Lecture {
-        int pay;
-        int day;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
 
-        public Lecture(int pay, int day) {
-            this.pay = pay;
-            this.day = day;
-        }
-    }
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        StringTokenizer st;
-        
-        int n = Integer.parseInt(br.readLine());
-        Lecture[] lectureArray = new Lecture[n];
-
-        for(int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
-            int pay = Integer.parseInt(st.nextToken());
-            int day = Integer.parseInt(st.nextToken());
-            lectureArray[i] = new Lecture(pay, day);
-        }
-
-        Arrays.sort(lectureArray, Comparator.comparingInt((Lecture lec) -> lec.day).thenComparingInt((Lecture lec) -> -lec.pay));
-
-        for(Lecture lecture : lectureArray){
-            if(queue.size() < lecture.day) queue.offer(lecture.pay);
-            else if(!queue.isEmpty() && queue.peek() < lecture.pay){
-                queue.poll();
-                queue.offer(lecture.pay);
-            }
-        }
-
+    public static void main(String[] args) throws IOException{
+        int N = Integer.parseInt(br.readLine());
+        Lecture[] array = new Lecture[N];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         int result = 0;
 
-        while(!queue.isEmpty()){
-            result += queue.poll();
+        for(int i = 0;i < N;i++) {
+            st = new StringTokenizer(br.readLine());
+            array[i] = new Lecture(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+        
+        Arrays.sort(array);
+        
+        for(Lecture l : array){
+            pq.add(l.pay);
+            if(l.day < pq.size()) pq.poll();
         }
 
-        System.out.println(result);
+        while(!pq.isEmpty()) result += pq.poll();
+        sb.append(result);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
