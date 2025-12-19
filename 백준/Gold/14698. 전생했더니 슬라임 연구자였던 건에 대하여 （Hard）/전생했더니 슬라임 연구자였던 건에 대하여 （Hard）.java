85f8c9c3f1326ae9b8cdb8af4c030beba793cd84
JoonYong Lim <feyfey927@gmail.com>
2025-12-20 00:36:29 +0900

    [Gold IV] Title: 전생했더니 슬라임 연구자였던 건에 대하여 (Hard), Time: 1040 ms, Memory: 154372 KB -BaekjoonHub

--- 백준/Gold/14698. 전생했더니 슬라임 연구자였던 건에 대하여 （Hard）/전생했더니 슬라임 연구자였던 건에 대하여 （Hard）.java
+++ 백준/Gold/14698. 전생했더니 슬라임 연구자였던 건에 대하여 （Hard）/전생했더니 슬라임 연구자였던 건에 대하여 （Hard）.java
@@ -1,42 +1,34 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
 
-        long division = 1000000007;
-        int caseNum = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException{
+        int testCase = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < caseNum; i++){
-            PriorityQueue<Long> queue = new PriorityQueue<>();
-            int num = Integer.parseInt(br.readLine());
+        for(int t = 0;t < testCase;t++){
+            int N = Integer.parseInt(br.readLine());
+            long result = 1;
+            PriorityQueue<Long> pq = new PriorityQueue<>();
+
             st = new StringTokenizer(br.readLine());
-
-            for(int n = 0; n < num; n++){
-                queue.add(Long.parseLong(st.nextToken()));
+            for(int i = 0;i < N;i++) pq.add(Long.parseLong(st.nextToken()));
+            while(pq.size() > 1){
+                long first = pq.poll();
+                long second = pq.poll();
+                long target = (first * second) % 1000000007;
+                result = (result * target) % 1000000007;
+                pq.add(first * second);
             }
 
-            long totalCost = 1;
-
-            while(queue.size() > 1){
-                long a = queue.poll();
-                long b = queue.poll();
-                long cost = (a * b) % division;
-                totalCost = (totalCost * cost) % division;
-                queue.add(a * b);
-            }
-
-            bw.write(totalCost + "\n");
+            sb.append(result).append("\n");
         }
 
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
