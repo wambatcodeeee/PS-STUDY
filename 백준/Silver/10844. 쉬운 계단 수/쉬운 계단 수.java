73bff27ca28993cb0917efb55c99b18f43b63429
JoonYong Lim <feyfey927@gmail.com>
2026-01-28 23:35:55 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 92 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        int N = Integer.parseInt(br.readLine());
+        long result = 0;
+        long[][] dp = new long[N + 1][10];
+        for(int i = 1;i < 10;i++) dp[1][i] = 1;
+
+        for(int i = 2;i <= N;i++){
+            for(int j = 0;j < 10;j++){
+                if(j == 0) dp[i][0] = dp[i - 1][1] % 1000000000;
+                else if(j == 9) dp[i][9] = dp[i - 1][8] % 1000000000;
+                else{
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+                }
+            }
+        }
+
+        for(int i = 0;i < 10;i++) result += dp[N][i];
+        sb.append(result % 1000000000);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
